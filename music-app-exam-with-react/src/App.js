196f7ee69fe7658d5d62dd84988ef37a2b95af36
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-22 20:01:18 +0300

    routing completed

--- music-app-exam-with-react/src/App.js
+++ music-app-exam-with-react/src/App.js
@@ -15,22 +15,22 @@ function App() {
   return (
     <div className="App">
 
-      <Header />
 
-      <main id="main-content">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Welcome />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/albums' element={<ListAlbums />} />
-            <Route path='/albums/create' element={<CreateAlbum />} />
-            <Route path='/albums/:id' element={<AlbumDetails />} />
-            <Route path='/albums/edit/:id' element={<EditAlbum />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
-  
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/albums' element={<ListAlbums />} />
+          <Route path='/albums/create' element={<CreateAlbum />} />
+          <Route path='/albums/:id' element={<AlbumDetails />} />
+          <Route path='/albums/edit/:id' element={<EditAlbum />} />
+          <Route path='/albums/search' element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+
       <Footer />
 
 
--- music-app-exam-with-react/src/components/Header/Header.js
+++ music-app-exam-with-react/src/components/Header/Header.js
@@ -1,15 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <nav>
         <img src="./images/headphones.png" />
-        <a href="#">Home</a>
+        <Link to="/">Home</Link>
         <ul>
-            <li><a href="#">Catalog</a></li>
+            <Link to="/albums">Catalog</Link>
+            <Link to="/albums/search">Search</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/albums/create">Create Album</Link>
+            <Link to="/logout">Logout</Link>
+            {/* <li><a href="#">Catalog</a></li>
             <li><a href="#">Search</a></li>
             <li><a href="#">Login</a></li>
             <li><a href="#">Register</a></li>
             <li><a href="#">Create Album</a></li>
-            <li><a href="#">Logout</a></li>
+            <li><a href="#">Logout</a></li> */}
         </ul>
     </nav>
     )
